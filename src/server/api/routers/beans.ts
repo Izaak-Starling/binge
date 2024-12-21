f0a71380b61cdd23b2431f0d0495c34b55affd05
Izaak-Starling <izaak.birchall@starlingbank.com>
2024-12-21 14:39:49 +0000

    Store bean orders on receiving

--- src/server/api/routers/beans.ts
+++ src/server/api/routers/beans.ts
@@ -3,6 +3,13 @@ import {z} from "zod";
 
 export const t = initTRPC.create();
 
+interface BeanOrder {
+  beanId: string;
+  name: string;
+}
+
+const beanOrders: BeanOrder[] = [];
+
 export const beanRouter = t.router({
   orderBeans: t.procedure
   .input(
@@ -13,9 +20,11 @@ export const beanRouter = t.router({
   )
   .mutation(async (opts) => {
     const {input} = opts;
+
+    beanOrders.push({
+      beanId: input.beanId,
+      name: input.user
+    })
     console.log("Ordering beans " + input.beanId + " for user " + input.user);
-    return {
-      text: "Ordering beans " + input.beanId + " for user " + input.user,
-    };
   }),
 });
