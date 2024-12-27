df110e5dee50bb17ba111114849020a2553464f7
Izaak-Starling <izaak.birchall@starlingbank.com>
2024-12-27 18:02:30 +0000

    Set colours on order history by state type

--- src/app/_components/orderHistory/orderCard.tsx
+++ src/app/_components/orderHistory/orderCard.tsx
@@ -4,29 +4,36 @@ import React from "react";
 
 const OrderCard = (props: { order: BeanOrder }) => {
 
-  const mapState = (state: OrderState): string => {
-    switch (state) {
-      case OrderState.Pending:
-        return "Pending";
-      case OrderState.Accepted:
-        return "Accepted";
-      case OrderState.Completed:
-        return "Completed";
-      case OrderState.Cancelled:
-        return "Cancelled";
-    }
-    return "Unknown";
+  let dropShadowStyle = "";
+  let orderStateString = "";
+
+  switch (props.order.orderState) {
+    case OrderState.Pending:
+      orderStateString = "Pending";
+      break;
+    case OrderState.Accepted:
+      dropShadowStyle = "shadow-yellow-700";
+      orderStateString = "Accepted";
+      break;
+    case OrderState.Completed:
+      dropShadowStyle = "shadow-green-700";
+      orderStateString = "Completed";
+      break;
+    case OrderState.Cancelled:
+      dropShadowStyle = "shadow-red-700";
+      orderStateString = "Cancelled";
+      break;
   }
 
   return (
       <div
-          className={"max-w-sm rounded-lg bg-origin-padding overflow-hidden shadow-lg border-binge-off-black border-2 mt-5" + (props.order.orderState === OrderState.Cancelled ? " shadow-red-700" : "")}
+          className={dropShadowStyle + " max-w-sm rounded-lg bg-origin-padding overflow-hidden shadow-lg border-binge-off-black border-2 mt-5"}
           key={props.order.orderId}>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{props.order.beanName}</div>
           <p className="text-gray-700 text-base">
             Placed {minsSinceDate(props.order.orderPlacedDateTime)} minutes ago<br/>
-            In State: {mapState(props.order.orderState)}
+            In State: {orderStateString}
           </p>
         </div>
       </div>
