1cc761cf58bc6699af134c8bfd9ae8134e80f4d3
Izaak-Starling <izaak.birchall@starlingbank.com>
2024-12-27 17:41:41 +0000

    Fix ssr error on history page & add back button

--- src/app/_components/orderHistory/OrderHistoryPage.tsx
+++ src/app/_components/orderHistory/OrderHistoryPage.tsx
@@ -2,31 +2,45 @@
 
 import {api} from "~/trpc/react";
 import OrderCard from "~/app/_components/orderHistory/orderCard";
+import React, {useEffect, useState} from "react";
+import {LuBean} from "react-icons/lu";
 
 // TODO: Add a back button
 const OrderHistoryPage = () => {
-  let userName = "";
+  const [userName, setUserName] = useState("");
 
-  if (typeof window === 'object') {
-    userName = window.localStorage.getItem("name") ?? "";
-  }
+  useEffect(() => {
+    if (typeof window === 'object') {
+      setUserName(window.localStorage.getItem("name") ?? "");
+    }
+  })
 
   const {data: orders = []} = api.bean.getBeanOrdersByName.useQuery({name: userName});
-  if (userName) {
-
+  if (!userName) {
     return (
+        <p>Username not set</p>
+    );
+  }
+
+  return (
+      <>
+        {orders.length === 0 ? (<p>You dont have any matches :(</p>) : ""}
         <div>
           {(orders.map(order => (
               <OrderCard key={order.orderId} order={order}/>
           )))}
         </div>
-    );
-  }
-
-  return (
-      <p>Username not set</p>
+        <footer className="footer footer-center w-screen fixed bottom-0">
+          <div className="flex justify-center pb-2">
+            <a href="/swipe">
+              <div className="p-4 rounded-full shadow">
+                <LuBean size="1.5em"/>
+              </div>
+            </a>
+          </div>
+        </footer>
+      </>
   );
-
 }
 
 export default OrderHistoryPage;
