58e232a42beb3cdaaf6b1dccf4b5f4eded3cb261
Izaak-Starling <izaak.birchall@starlingbank.com>
2024-12-27 21:35:52 +0000

    remove dev indicators

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
+};
+
 
 export default config;
