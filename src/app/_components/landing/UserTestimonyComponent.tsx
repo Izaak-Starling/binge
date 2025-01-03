e04ca39bd9d88f27465c71eaf49cbd3b41d418c4
Izaak-Starling <izaak.birchall@starlingbank.com>
2025-01-03 20:51:17 +0000

    Add the testimony component without carousel

--- src/app/_components/landing/UserTestimonyComponent.tsx
+++ src/app/_components/landing/UserTestimonyComponent.tsx
@@ -1,7 +1,7 @@
 const UserTestimonyComponent = () => {
 
   return (
-      <div className="bg-binge-off-black h-screen">
+      <div className="bg-binge-off-black h-screen/2">
         <div className="relative isolate px-6 pt-1 lg:px-8">
           <div className="mx-auto max-w-2xl py-0 sm:py-48 lg:py-56">
 
@@ -9,6 +9,14 @@ const UserTestimonyComponent = () => {
               What Our Users Say
             </p>
 
+            <div className="h-full" data-carousel="slide">
+
+              <div className="mt-48 mb-4">
+                <p className="text-3xl text-binge-off-white font-serif">"Thank you Binge, we&apos;re getting married in a few months!"</p>
+                <p className="text-1xl text-binge-off-white pt-6">Aurellio & Insert any person at party</p>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
